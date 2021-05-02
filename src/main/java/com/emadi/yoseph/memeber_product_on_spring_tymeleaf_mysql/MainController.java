b1e469840af4f4e56ba82aea1397290226b992ef
Yousef-Emadi <Rahaa1400@gmail.com>
2021-05-01 20:29:30 -0400

    so fat so good

--- src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/MainController.java
+++ src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/MainController.java
@@ -15,19 +15,35 @@ public class MainController {
     @ResponseBody
     public String addNewMember(@RequestParam String username, @RequestParam String password, @RequestParam String name_first, @RequestParam String name_last, @RequestParam String phone, @RequestParam String email) {
         Member newMember = new Member();
-        newMember.setName_first(name_first);
-        newMember.setName_last(name_last);
-        newMember.setUsername(username);
+        newMember.setName_first(name_first.toLowerCase());
+        newMember.setName_last(name_last.toLowerCase());
+        newMember.setUsername(username.toLowerCase());
         newMember.setPassword(password);
         newMember.setPhone(phone);
-        newMember.setEmail(email);
+        newMember.setEmail(email.toLowerCase());
         newMember.setBalance(100);
         memberRepository.save(newMember);
-        return "Now You are a part of our members family";
+        return "Welcome dear " + name_first +"! \nFrom now on, You are a part of our family. &#129303";
     }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login(@RequestParam String username, @RequestParam String password){
+        for (Member member: memberRepository.findAll()
+             ) {
+            if ((member.username.compareToIgnoreCase(username) == 0) && (member.password.compareTo(password) == 0)){
+                return "welcome Back " + member.name_first + " ! &#128522"  ;
+            }
+        }
+        return "Username or password is incorrect! &#129300";
+    }
+
+
 
     @GetMapping("/list")
     public @ResponseBody Iterable<Member> listAllMembers(){
         return memberRepository.findAll();
     }
+
+
 }
