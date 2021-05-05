efb24089e2771a569a3eec6c8a1b41c49881f8e8
Yousef-Emadi <Rahaa1400@gmail.com>
2021-05-05 13:03:33 -0400

    so far so good

--- src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/Service.java
+++ src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/Service.java
@@ -19,4 +19,44 @@ public class Service {
     String description;
     String category;
     double price;
+
+    //Constructors:
+    public Service() {
+    }
+
+    //getters and setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
+
