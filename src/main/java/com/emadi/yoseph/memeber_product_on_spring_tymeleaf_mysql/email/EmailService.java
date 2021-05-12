dea9203bf513e7518827c6fb0f3e28e9934e5a48
Yousef-Emadi <Rahaa1400@gmail.com>
2021-05-11 22:16:08 -0400

    some comments

--- src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/email/EmailService.java
+++ src/main/java/com/emadi/yoseph/memeber_product_on_spring_tymeleaf_mysql/email/EmailService.java
@@ -4,6 +4,13 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+/**
+ * Project: Member Database with Hibernate
+ * Developed by: Yousef Emadi
+ * Date: APR 2021
+ * Depends on: Maven, Spring, Hibernate, Thymeleaf, MySQL
+ */
+
 @Service
 public class EmailService {
 
